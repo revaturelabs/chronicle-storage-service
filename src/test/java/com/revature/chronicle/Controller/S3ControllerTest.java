74077cff0047c93e71f09381df35f92e6bfb5b37
Thomas Gage <57278497+tgage211996@users.noreply.github.com>
2020-12-24 20:14:01 -0600

    undid my controller changes

--- src/test/java/com/revature/chronicle/Controller/S3ControllerTest.java
+++ src/test/java/com/revature/chronicle/Controller/S3ControllerTest.java
@@ -2,9 +2,7 @@ package com.revature.chronicle.Controller;
 
 
 
-import com.google.common.collect.Multimap;
-import netscape.javascript.JSObject;
-import org.json.JSONException;
+
 import org.json.JSONObject;
 import org.junit.*;
 import org.junit.runner.RunWith;
@@ -16,22 +14,17 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
+
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -43,55 +36,39 @@ public class S3ControllerTest {
 	//Mocker object
 	private MockMvc mvc;
 
-	private MockMultipartFile file;
-	private MockMultipartFile file2;
-
-	private JSONObject json = new JSONObject();
-
-	final MultiValueMap<String, String> params  = new LinkedMultiValueMap<>();
-
 	@Autowired
 	private WebApplicationContext webApplicationContext;
 
-
+	private MockMultipartFile file;
 
 
 	@Before
-	public void setup() throws JSONException, IOException {
-
-		//given a mock MultipartFile an existing file to pass into the controller
-		file = file = new MockMultipartFile(
-				"file",
-				"test.txt",
-				MediaType.TEXT_PLAIN_VALUE,
-				"Hello, World!".getBytes()
-		);
-
-		//given a mock MultipartFile with a non existent file to pass into the controller
-		file2 = new MockMultipartFile(
-				"file",
-				"falsefilename",
-				MediaType.TEXT_PLAIN_VALUE,
-				"Hello, World!".getBytes()
-		);
-
-		//mocking the json description from front end
-		 json.put("testing", "123");
-		 json.put("test2","here we go");
-
-
-		//this multivalue map is mocking the passing of a file and json description as parameters into the servlet
-		params.add("file", file.getBytes().toString());
-		params.add("json", json.toString());
-
-
-
+	public void setup(){
 		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
 
 	@Test
 	public void givenFormData_whenFileUpload_theReturnOKString() throws Exception {
+
+		//given a mock MultipartFile to pass into the controller
+		file = new MockMultipartFile(
+				"file",
+				"test.txt",
+				MediaType.TEXT_PLAIN_VALUE,
+				"Hello, World!".getBytes()
+		);
+
+		//mocking the json discription from front end
+		JSONObject json = new JSONObject();
+		json.put("testing", "123");
+		json.put("test2","here we go");
+
+		//this multivalue map is mocking the passing of a file and json description as parameters into the servlet
+		final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("file", file.getOriginalFilename());
+		params.add("json", json.toString());
+
 
 		//this is mocking the servlet being called and being passed the needed parameters for desired services
 		//when
@@ -113,6 +90,28 @@ public class S3ControllerTest {
 
 	@Test
 	public void givenBadFilePath_WhenFileUpload_shouldReturnError() throws Exception {
+
+	//given a mock MultipartFile to pass into the controller
+	file = new MockMultipartFile(
+				"file",
+				"falsefilename",
+				MediaType.TEXT_PLAIN_VALUE,
+				"Hello, World!".getBytes()
+		);
+
+	//mocking the json discription from front end
+	JSONObject json = new JSONObject();
+	json.put("testing", "123");
+	json.put("test2","here we go");
+
+	HttpServletResponse httpServletResponse = new MockHttpServletResponse();
+	httpServletResponse.setStatus(404);
+
+	//this multivalue map is mocking the passing of a file and json description as parameters into the servlet
+	final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+	params.add("file", (file.getBytes().toString()));
+	params.add("json", json.toString());
+
 
 	//this is mocking the servlet being called and being passed the needed parameters for desired services
 	//when
