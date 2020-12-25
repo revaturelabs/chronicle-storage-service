c010102b598305ada100aa97840b485e5bbb4c06
Justin Kroh <justinkroh22@gmail.com>
2020-12-24 19:53:27 -0500

    Added Temporary Cors Fix -- Cors Allows Everything

--- src/main/java/com/revature/chronicle/ChronicleApplication.java
+++ src/main/java/com/revature/chronicle/ChronicleApplication.java
@@ -2,9 +2,12 @@ package com.revature.chronicle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
@@ -14,8 +17,12 @@ public class ChronicleApplication {
 		SpringApplication.run(ChronicleApplication.class, args);
 	}
 
-	@GetMapping("/hello")
+	@CrossOrigin
+	@GetMapping(path = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		
+		System.out.println("IT worked");
+		
 		return String.format("Hello %s!", name);
 	}
 }
--- src/main/java/com/revature/chronicle/security/SecurityConfig.java
+++ src/main/java/com/revature/chronicle/security/SecurityConfig.java
@@ -3,13 +3,45 @@ package com.revature.chronicle.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.revature.chronicle.security.CorsConfigurationProps;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	private final CorsConfigurationProps corsConfigurationProps;
+	
+	
 
-    @Override
+    public SecurityConfig(CorsConfigurationProps corsConfigurationProps) {
+		this.corsConfigurationProps = corsConfigurationProps;
+	}
+
+
+
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+        	.cors(spec -> {
+	            CorsConfiguration config = new CorsConfiguration();
+        		config.setAllowCredentials(true);
+	            config.addAllowedOriginPattern("*");
+	            config.addAllowedHeader("*");
+	    		config.addAllowedMethod("*");
+	            
+
+//	            config.setAllowedMethods(corsConfigurationProps.getAllowedMethods());
+//	            config.setAllowedHeaders(corsConfigurationProps.getAllowedHeaders());
+//	            config.setExposedHeaders(corsConfigurationProps.getExposedHeaders());
+//	            config.setAllowCredentials(corsConfigurationProps.isAllowCredentials());
+	            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	            source.registerCorsConfiguration("/**", config);
+	            spec.configurationSource(source);
+        		})
+        		.csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
