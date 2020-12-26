ef6b3764473935a23bb690a50548bc9f601e396b
UNDERHMA <underhillmason@gmail.com>
2020-12-26 12:58:50 -0500

    Removed csrf.disable() from httpsecurity config

--- src/main/java/com/revature/chronicle/security/SecurityConfig.java
+++ src/main/java/com/revature/chronicle/security/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
                 .cors().configurationSource(corsConfigurationSource())
                 .and()
                 .authorizeRequests()
