9e8e8716bafcfa068bebff59f755ccf2763fd7f8
RonaldMartz <ronald.j.martz@gmail.com>
2020-12-28 18:28:33 -0600

    Wrote tests for Media Retrieval Controller

--- src/test/java/com/revature/chronicle/ChronicleApplicationTests.java
+++ src/test/java/com/revature/chronicle/ChronicleApplicationTests.java
@@ -1,13 +1,71 @@
 package com.revature.chronicle;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class ChronicleApplicationTests {
 
+	private vidoeDao vd;
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup()
+	{
+		vd = mock(videoDao.class);
+	}
+
 	@Test
-	void contextLoads() {
+	public void shouldGetAllVideos() throws Exception {
+		MvcResult result = mockMvc.perform(get("/videos/all"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andReturn();
+
+		int actual = result.getResponse().getStatus();
+
+		int expected = 200;
+
+		Assert.assertEquals(actual, expected); //Testing to ensure the get request was successful
+
+		//Testing to ensure something is being returned
+		Assert.assertTrue(result.getResponse().getContentAsString().length() > 0);
+
+		//Testing to ensure the response is formatted properly
+		Assert.assertNotNull(result.getResponse().getHeader("Content-Type").equals("application/json;charset=UTF-8"));
+	}
+
+	@Test
+	public void shouldGetVideosByTag() throws Exception {
+		MvcResult result = mockMvc.perform(get("/videos/tags/{videoTags}","Java+JDBC"))//Assuming words separated by '+'
+				.andExpect(status().isOk())
+				.andReturn();
+
+		int actual = result.getResponse().getStatus();
+
+		int expected = 200;
+
+		Assert.assertEquals(actual, expected); //Testing to ensure the get request was successful
+
+		//Testing to ensure something is being returned
+		Assert.assertTrue(result.getResponse().getContentAsString().length() > 0);
+
+		//Testing to ensure the response is formatted properly
+		Assert.assertNotNull(result.getResponse().getHeader("Content-Type").equals("application/json;charset=UTF-8"));
 	}
 
 }
