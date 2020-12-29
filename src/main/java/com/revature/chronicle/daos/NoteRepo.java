f41757f8c6da8491c4fc76393916f049150a7661
Timothy Cahill <tcahill17@hotmail.com>
2020-12-28 23:50:43 -0500

    Tested functionality of generated repo methods and added comments to specify their equivalent methods in the repo.

--- src/main/java/com/revature/chronicle/daos/NoteRepo.java
+++ src/main/java/com/revature/chronicle/daos/NoteRepo.java
@@ -2,20 +2,24 @@ package com.revature.chronicle.daos;
 
 import com.revature.chronicle.models.Note;
 import com.revature.chronicle.models.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 /**
- * Data access object interface for note data
+ * Repository interface for note data
  */
-public interface NoteRepo {
-    Note getNote(int id);
-    List<Note> getNotes();
-    List<Note> getNotes(Tag tag);
+public interface NoteRepo extends JpaRepository<Note, Integer> {
+//    // Needs to be implemented
+//    List<Note> findByTag(Tag tag);                    Causes error when not commented out.
+//    boolean addTag(Note note, Tag tag);               Causes error when not commented out.
+//    boolean removeTag(Note note, Tag tag);            Causes error when not commented out.
+//
+//    // Implemented and functional
+//    Note getNote(int id);   ->   Optional<Note> findById(Integer id);
+//    List<Note> getNotes();    ->     List<Note> findAll();
+//    boolean addNote(Note note);         ->     void save();
+//    boolean updateNote(Note note);         ->     void save();
+//    boolean deleteNote(Note note);        ->    void delete();
 
-    boolean addNote(Note note);
-    boolean updateNote(Note note);
-    boolean deleteNote(Note note);
-    boolean addTag(Note note, Tag tag);
-    boolean removeTag(Note note, Tag tag);
 }
