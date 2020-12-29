c7f3211a12949d1bcd68d4314156076da7016523
javierg52 <javier.gonzalez@revature.net>
2020-12-29 01:03:06 -0500

    Created InsertFileService class for tests

---
+++ src/main/java/com/revature/chronicle/services/InsertFileService.java
@@ -0,0 +1,11 @@
+package com.revature.chronicle.services;
+
+public class InsertFileService {
+    public Object insertFile(String url, String processedForm) {
+        return null;
+    }
+
+    public boolean addSuccess() {
+        return false;
+    }
+}
