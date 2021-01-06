74b71471cc6d0046b0ac89e02f6b8f229905bb25
Cassie C <cassie@colvindesign.com>
2021-01-06 12:26:38 -0500

    Added tests for VideoController and NoteService. Got around spring secuirty for controller test.

--- src/test/java/com/revature/chronicle/NoteServiceTests.java
+++ src/test/java/com/revature/chronicle/NoteServiceTests.java
@@ -1,11 +1,18 @@
 package com.revature.chronicle;
 
+import com.amazonaws.services.dynamodbv2.xspec.N;
 import com.revature.chronicle.daos.NoteRepo;
+import com.revature.chronicle.models.Note;
+import com.revature.chronicle.models.Tag;
+import com.revature.chronicle.models.User;
 import com.revature.chronicle.services.NoteService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,27 +27,51 @@ public class NoteServiceTests {
 
     @Test
     public void shouldReturnAListOfAllNotes(){
+        List<Note> notes = new ArrayList();
+        notes.add(new Note(1,"www.note.com","A description",new Date(), new User(), new HashSet<Tag>()));
+
+        when(repo.findAll()).thenReturn(notes);
+
+        List<Note> result = service.findAll();
+
+        Assert.assertTrue(notes.equals(result));
+
+        verify(repo).findAll();
 
     }
 
     @Test
     public void shouldReturnANoteById(){
-
+        Note note = new Note(1,"www.note.com","a description",new Date(),new User(), new HashSet<Tag>());
+        when(repo.findById(1)).thenReturn(Optional.of(note));
+        Note result = service.findById(1);
+        Assert.assertTrue(note.equals(result));
+        verify(repo).findById(1);
     }
 
     @Test
-    public void shouldReturnNullIfNoNoteoFound(){
-
+    public void shouldReturnNullIfNoNoteFound(){
+        when(repo.findById(2)).thenReturn(Optional.empty());
+        Note result = service.findById(2);
+        Assert.assertFalse(result==null);
+        verify(repo).findById(2);
     }
 
     @Test
     public void shouldSaveANoteAndReturnTrue(){
-
+        Note note = new Note(1,"www.note.com","a description",new Date(),new User(), new HashSet<Tag>());
+        when(repo.save(note)).thenReturn(note);
+        boolean result = service.save(note);
+        Assert.assertTrue(result);
+        verify(repo).save(note);
     }
 
     @Test
     public void shouldFailToAddNoteAndReturnFalse(){
-
+        when(repo.save(null)).thenReturn(null);
+        boolean result = service.save(null);
+        Assert.assertFalse(result);
+        verify(repo).save(null);
     }
 
     @Test
