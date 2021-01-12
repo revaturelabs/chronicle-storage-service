df91b3cf07318ca2ae0c421102b11de2c7764643
Javier Gonzalez <sycotickid5@gmail.com>
2021-01-12 15:13:39 -0500

    Update NoteControllerTests.java

--- src/test/java/com/revature/chronicle/controller/NoteControllerTests.java
+++ src/test/java/com/revature/chronicle/controller/NoteControllerTests.java
@@ -1,4 +1,4 @@
-package com.revature.chronicle.Controller;
+package com.revature.chronicle.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.chronicle.daos.TagRepo;
