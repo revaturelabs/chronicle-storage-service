f0d402d74b9a7cc6a8dd01c449fa85079959c6d1
Javier Gonzalez <javier.gonzalez@revature.com>
2021-01-20 19:12:23 -0500

    Final changes to FileUploadController as well as import statement cleaning in each class

--- src/main/java/com/revature/chronicle/services/UserService.java
+++ src/main/java/com/revature/chronicle/services/UserService.java
@@ -2,7 +2,6 @@ package com.revature.chronicle.services;
 
 import com.revature.chronicle.daos.UserRepo;
 import com.revature.chronicle.models.User;
-import com.revature.chronicle.models.Video;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
