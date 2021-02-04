9e68f16455141600f77341ec028229d4160ea1f9
George Yoo <gcyoosf@gmail.com>
2021-02-04 14:21:20 -0500

    resets back to original state

--- src/main/java/com/revature/chronicle/controller/VideoController.java
+++ src/main/java/com/revature/chronicle/controller/VideoController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +22,7 @@ import com.revature.chronicle.models.Video;
 import com.revature.chronicle.services.VideoService;
 
 @RestController
-@CrossOrigin(origins = "*", allowCredentials = "true")
+//@CrossOrigin(origins = "*", allowCredentials = "true")
 @RequestMapping(path = "/videos")
 public class VideoController {
 
