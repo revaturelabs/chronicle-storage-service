00b9e4a373c89745edf0612482d7babcb28d825b
James Olas <jamesolas@gmail.com>
2021-03-29 14:53:18 -0700

    notificaiton repo

--- src/main/java/com/revature/chronicle/daos/NotificationRepo.java
+++ src/main/java/com/revature/chronicle/daos/NotificationRepo.java
@@ -1,7 +1,12 @@
 package com.revature.chronicle.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.google.api.services.storage.model.Notification;
+@Repository(value="notificationRepository")
 public interface NotificationRepo extends JpaRepository<Notification, Integer> {
+
+		Notification findByTicketid(int ticketid);
 
 }
