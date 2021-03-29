9f83e05ed7a5bb1aeb9e4bda47884a778182647e
James Olas <jamesolas@gmail.com>
2021-03-29 14:48:08 -0700

    notification

---
+++ src/main/java/com/revature/chronicle/daos/NotificationRepo.java
@@ -0,0 +1,7 @@
+package com.revature.chronicle.daos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepo extends JpaRepository<Notification, Integer> {
+
+}
