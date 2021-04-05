dd8c3744ad12bdd6fbaf8b6566ae7342aeab4635
Michael Pearson <mpearson2516@yahoo.com>
2021-04-05 12:34:04 -0700

    Added some dummy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,17 +3,23 @@ insert into tag values (2, 'Topic', 'Spring');
 insert into tag values (3, 'Batch', '1120-August');
 insert into tag values (4, 'Batch', '1020-Ben');
 insert into tag values (5, 'Topic', 'Typescript');
+insert into tag values (6, 'Topic', 'JAVA Inheritance');
+insert into tag values (7, 'Topic', 'JAVA Collection');
 
 insert into video values (6,'2021-02-12 10:32:33.179', 'Created a basic Angular application and went over TypeScript OOP, data types, and Basics. Introduced data interpolation and NodeJS.', 'Jim' ,false, 'Angular and TypeScript Overview', 
 	  'https://chronicle-p3.s3.amazonaws.com/sample-mp4-file.mp4', 'temp_user');
 insert into video values (7,'2021-02-12 10:32:33.179', 'Setup a basic Spring Boot web application with rest-controllers and H2 database. Demonstrated Spring Data capabilities via api calls to controller methods', 'Jim2', false, 'Spring Boot Overview and Setup',  
 	  'https://chronicle-p3.s3.amazonaws.com/sample-mp4-file.mp4', 'temp_user');
+insert into video values (8,'2021-02-12 10:32:33.179', 'Demonstrating Java Inheritance', 'Jim3', false, 'JAVA Inheritance', 'https://chronicle-p3.s3.amazonaws.com/sample-mp4-file.mp4', 'temp_user');
+insert into video values (9,'2021-02-12 10:32:33.179', 'Demonstrating Java Collections', 'Jim4', false, 'JAVA Collection', 'https://chronicle-p3.s3.amazonaws.com/sample-mp4-file.mp4', 'temp_user');
 
 insert into video_tag values (6, 1);
 insert into video_tag values (6, 3);
 insert into video_tag values (7, 5);
 insert into video_tag values (6, 2);
 insert into video_tag values (7, 4);
+insert into video_tag values (8, 6);
+insert into video_tag values (9, 7);
 	
 insert into note values (8, '2021-02-12 10:32:33.179', 'Created a basic Angular application and went over TypeScript OOP, data types, and Basics. Introduced data interpolation and NodeJS.', 'John', false, 'Angular and TypeScript Overview', 
 	   'http://www.africau.edu/images/default/sample.pdf','temp_user');
